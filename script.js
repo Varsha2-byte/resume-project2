46ec835adeaa49b2a1cea54a8dc1b829deeaa1d4
Varsha2-byte <npatelvarsha@gmail.com>
2025-11-17 23:43:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-function showOutput() {
-    let name = document.getElementById("name").value;
-    let gender = document.getElementById("gender").value;
-    let phone = document.getElementById("phone").value;
-    let email = document.getElementById("email").value;
+$(document).ready(function () {
+    $("#regForm").submit(function () {
+        alert("Form submitted successfully!");
+        return true;
+    });
+});
 
-    document.getElementById("output").innerHTML = `
-        <h3>Submitted Details</h3>
-        <p><b>Name:</b> ${name}</p>
-        <p><b>Gender:</b> ${gender}</p>
-        <p><b>Phone:</b> ${phone}</p>
-        <p><b>Email:</b> ${email}</p>
-    `;
-}
